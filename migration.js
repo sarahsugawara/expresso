c030d1f3152c4b52ff3b0cb5d9098bdfefd4556f
Sarah Sugawara <pianistsf@gmail.com>
2018-08-20 20:18:15 -0400

    Downloaded dependencies. Created file structure for API

---
+++ migration.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+db.serialize(() => {
+    //Creating "Employee" table
+    db.run("CREATE TABLE IF NOT EXISTS Employee (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, position TEXT NOT NULL, wage INTEGER NOT NULL, is_current_employee INTEGER  NOT NULL DEFAULT 1);");
+    
+    //Creating "Timesheet" table
+    db.run("CREATE TABLE IF NOT EXISTS Series (id INTEGER PRIMARY KEY NOT NULL, hours INTEGER NOT NULL, rate INTEGER NOT NULL, date INTEGER NOT NULL, employee_id INTEGER NOT NULL, FOREIGN KEY (employee_id) REFERENCES Employee(id) );");
+
+    //Creating "Menu" table
+    db.run("CREATE TABLE IF NOT EXISTS Menu (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL);");
+
+    //Creating "MenuItem" table
+    db.run("CREATE TABLE IF NOT EXISTS MenuItem (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, description TEXT, inventory INTEGER NOT NULL, price INTEGER NOT NULL, menu_id INTEGER NOT NULL, FOREIGN KEY (menu_id) REFERENCES Menu(id) );");
+});
+
